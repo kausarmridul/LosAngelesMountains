68b12ba359471ce9f1f37bdd16e902fd054a708c
kausarmridul <mdmridul344@gmail.com>
2022-06-06 20:23:13 +0600

    File Add

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+})
